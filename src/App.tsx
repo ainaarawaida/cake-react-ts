eb131410acb692aa3940de7e950be8b5a92c588d
ainaarawaida <admin@ainaarawaida.com>
2024-04-09 07:11:06 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,44 @@
-import { useState } from 'react'
+import { useState, useRef, useLayoutEffect } from 'react'
+import Hello from "./Hello.svelte";
+import Counter from "./Counter.svelte";
+import useStore from "./store";
+
 import './App.css'
 
+function SvelteWrapper(Component:any) {
+  return (props:any) => {
+    const svelteRef = useRef();
+    useLayoutEffect(() => {
+      while (svelteRef.current?.firstChild) {
+        svelteRef.current?.firstChild?.remove();
+      }
+      new Component({
+        target: svelteRef.current,
+        props,
+      });
+    }, []);
+    return <div ref={svelteRef}></div>;
+  };
+}
+
+const SvelteHello = SvelteWrapper(Hello);
+const SvelteCounter = SvelteWrapper(Counter);
+
 function App() {
-  const [count, setCount] = useState(0)
+  const { count, increment } = useStore();
   const baseurl = (window as any)?.baseurl;
 
   return (
     <>
       <div>
+
+      <SvelteHello
+        extraText="This is a prop passed from React to Svelte"
+        onClick={increment}
+      />
+    
+      <SvelteCounter />
+
         <a href="https://book.cakephp.org/2/en/index.html" target="_blank">
           <img src={`${baseurl}images/cakephp.png`} className="logo" alt="cakePHP logo" />
         </a>
@@ -17,9 +48,9 @@ function App() {
       </div>
       <h1>CakePHP 2 + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <button className="btn btn-success" onClick={increment}>
+        count is {count}
+      </button>
         <p>
         Edit <code>app/React/src/App.tsx</code> and save to test HMR
         </p>
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 import liveReload from 'vite-plugin-live-reload';
 import path from 'path';
 
@@ -9,6 +10,7 @@ export default ({ mode }: { mode: string }) => {
   return defineConfig({
     plugins: [
       react(),
+      svelte(),
       liveReload([
         // // edit live reload paths according to your source code
         // // for example:
